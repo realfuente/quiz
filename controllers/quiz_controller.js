635087bd8ddfa8f9b66215531bf38cd3895ad6c1
realfuente <realfuente@hotmail.com>
2015-06-29 17:55:15 +0200

    Crear comentario

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,8 +11,14 @@ function replaceAll( text, busca, reemplaza ){
 
 // Autoload :id
 exports.load = function(req, res, next, quizId) {
-  models.Quiz.find(quizId).then(
-    function(quiz) {
+  models.Quiz.find({
+            where: {
+                id: Number(quizId)
+            },
+            include: [{
+                model: models.Comment
+            }]
+        }).then(function(quiz) {
       if (quiz) {
         req.quiz = quiz;
         next();
---
+++ models/comment.js
@@ -0,0 +1,12 @@
+// Definicion del modelo de Quiz con validación
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define(
+  	'Comment',
+    { texto: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "-> Falta Comentario"}}
+      }
+    }
+  );
+}
